086993f072ca75acd693bdd3c542e158b67e458f
Joseph Wardell <jaywardell@mac.com>
2022-02-08 18:55:55 -0500

    Initial Commit

---
+++ Shared/The_Big_3App.swift
@@ -0,0 +1,17 @@
+//
+//  The_Big_3App.swift
+//  Shared
+//
+//  Created by Joseph Wardell on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Big_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
