74785cdc1e226c52bb82264d53bf0cdf21e5b9b6
Joseph Wardell <jaywardell@mac.com>
2022-02-09 01:13:13 -0500

    removed old tests that are no longer relevant now that Planner passes the Plan's publisher on to the PlannerView's viewModel

--- ModelTestsMacOS/PlannerTests.swift
+++ ModelTestsMacOS/PlannerTests.swift
@@ -24,30 +24,4 @@ class PlannerTests: XCTestCase {
         
         XCTAssertEqual(sut.plan.allowed, 3)
     }
-
-    func test_planner_passes_changes_from_plan_on_to_subscribers() throws {
-        let sut = Planner()
-        
-        try expectChanges(on: sut, count: 1) {
-            try sut.plan.set("example", at: 0)
-        }
-    }
-    
-    // MARK: - Helpers
-    
-    private func expectChanges(on sut: Planner, count expected: Int, when callback: () throws ->(), file: StaticString = #filePath, line: UInt = #line) rethrows {
-        
-        var callCount = 0
-        var bag = Set<AnyCancellable>()
-        sut.objectWillChange.sink {
-            callCount += 1
-        }
-        .store(in: &bag)
-        
-        try callback()
-        
-        XCTAssertEqual(callCount, expected, file: file, line: line)
-
-    }
-
 }
