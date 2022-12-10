6c58ed72e67d85131fd13c4211d1ca0564ba51cb
Joseph Wardell <jaywardell@mac.com>
2022-12-10 06:12:08 -0500

    fixed a bunch of warnings caused by the change from struct to class for CompletionLog

--- The_Big_3Tests_iOS/CompletionLogTests.swift
+++ The_Big_3Tests_iOS/CompletionLogTests.swift
@@ -82,7 +82,7 @@ final class CompletionLogTests: XCTestCase {
     // MARK: - log
     
     func test_log_throws_if_goal_is_pending() {
-        var sut = makeSUT()
+        let sut = makeSUT()
         
         XCTAssertThrowsError(try sut.log(pendingGoal)) { error in
             XCTAssertEqual(error as? CompletionLog.Error, .GoalIsNotCompleted)
@@ -90,7 +90,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_log_throws_if_goal_is_deferred() {
-        var sut = makeSUT()
+        let sut = makeSUT()
         
         XCTAssertThrowsError(try sut.log(deferredGoal)) { error in
             XCTAssertEqual(error as? CompletionLog.Error, .GoalIsNotCompleted)
@@ -98,14 +98,14 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_log_does_not_throw_if_goal_is_completed() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         
         XCTAssertNoThrow(try sut.log(finishedGoal))
     }
     
     func test_log_calls_archive_record() throws {
         let spy = CompletionLogArchiveSpy()
-        var sut = makeSUT(archive: spy)
+        let sut = makeSUT(archive: spy)
         
         try sut.log(finishedGoal)
         
@@ -114,7 +114,7 @@ final class CompletionLogTests: XCTestCase {
     
     func test_log_passes_proper_title_to_record() throws {
         let spy = CompletionLogArchiveSpy()
-        var sut = makeSUT(archive: spy)
+        let sut = makeSUT(archive: spy)
         
         let toadd = finishedGoal
         let expected = toadd.title
@@ -126,7 +126,7 @@ final class CompletionLogTests: XCTestCase {
 
     func test_log_passes_proper_date_to_record() throws {
         let spy = CompletionLogArchiveSpy()
-        var sut = makeSUT(archive: spy)
+        let sut = makeSUT(archive: spy)
         
         let expected = Date().addingTimeInterval(5)
         
@@ -138,7 +138,7 @@ final class CompletionLogTests: XCTestCase {
     var cancellables = Set<AnyCancellable>()
 
     func test_log_triggers_publisher() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
 
 
         try expectChanges(for: sut.logChanged.eraseToAnyPublisher(), count: 1) {
@@ -147,7 +147,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_log_sends_new_days_to_publisher() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
 
         var found = [Date]()
 
@@ -165,7 +165,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_log_sends_new_days_to_publisher_even_if_day_is_already_logged_for_another_goal() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         try sut.log(finishedGoal2)
         
         var found = [Date]()
@@ -192,7 +192,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_days_is_updated_by_log() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         let date = Date()
         let expected = Calendar.current.startOfDay(for: date)
         
@@ -202,7 +202,7 @@ final class CompletionLogTests: XCTestCase {
     }
 
     func test_days_is_not_updated_if_another_goal_was_recorded_on_the_same_day() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         let date1 = Date()
         let date2 = Date().addingTimeInterval(1)
         let expected = [Calendar.current.startOfDay(for: date1)]
@@ -214,7 +214,7 @@ final class CompletionLogTests: XCTestCase {
     }
 
     func test_days_is_updated_if_recorded_on_a_new_day() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         let date1 = Date()
         let date2 = Date().addingTimeInterval(25*(3600))
         let expected = [
@@ -237,7 +237,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_dates_is_updated_by_log() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         let finished = finishedGoal
         let date = Date()
         
@@ -247,7 +247,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_dates_returns_goals_sorted_by_date_regardless_of_order_goals_were_logged() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         
         let goal1 = Plan.Goal(title: "1", state: .completed)
         let goal2 = Plan.Goal(title: "2", state: .completed)
@@ -274,7 +274,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_titleForGoal_return_title_of_goal_logged_for_fate_it_was_logged() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         let toLog = finishedGoal
         let date = Date()
         
@@ -284,7 +284,7 @@ final class CompletionLogTests: XCTestCase {
     }
     
     func test_titleForGoal_properly_returns_goal_for_date_regardless_of_order_logged() throws {
-        var sut = makeSUT()
+        let sut = makeSUT()
         
         let goal1 = Plan.Goal(title: "1", state: .completed)
         let goal2 = Plan.Goal(title: "2", state: .completed)
