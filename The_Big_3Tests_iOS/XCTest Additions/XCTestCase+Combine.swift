1086afbf3bbf66c830c578b7f1942b0f7f082fb9
Joseph Wardell <jaywardell@mac.com>
2022-12-10 11:19:51 -0500

    updated XCTestCase combine convenience methods to support multithreading.  This should have been done from the beginning, but I guess I didn't need it.

--- The_Big_3Tests_iOS/XCTest Additions/XCTestCase+Combine.swift
+++ The_Big_3Tests_iOS/XCTest Additions/XCTestCase+Combine.swift
@@ -13,13 +13,17 @@ extension XCTestCase {
     func expectChanges<Type>(for publisher: AnyPublisher<Type, Never>, count expected: Int, when callback: () async throws ->(), file: StaticString = #filePath, line: UInt = #line) async rethrows where Error == Error {
         
         var callCount = 0
+        let expectation = XCTestExpectation(description: "expect changes for publisher")
         var bag = Set<AnyCancellable>()
         publisher.sink { _ in
             callCount += 1
+            expectation.fulfill()
         }
         .store(in: &bag)
         
         try await callback()
+        
+        wait(for: [expectation], timeout: 1)
         
         XCTAssertEqual(callCount, expected, file: file, line: line)
     }
@@ -27,19 +31,38 @@ extension XCTestCase {
     func expectChanges<Type>(for publisher: AnyPublisher<Type, Never>, count expected: Int, when callback: () throws ->(), file: StaticString = #filePath, line: UInt = #line) rethrows where Error == Error {
         
         var callCount = 0
+        let expectation = XCTestExpectation(description: "expect changes for publisher")
         var bag = Set<AnyCancellable>()
         publisher.sink { _ in
             callCount += 1
+            expectation.fulfill()
         }
         .store(in: &bag)
         
         try callback()
         
+        wait(for: [expectation], timeout: 1)
+
         XCTAssertEqual(callCount, expected, file: file, line: line)
     }
     
     func expectNoChanges<Type>(for publisher: AnyPublisher<Type, Never>, when callback: () throws ->(), file: StaticString = #filePath, line: UInt = #line) rethrows {
-        try expectChanges(for: publisher, count: 0, when: callback)
+        
+        var callCount = 0
+        let expectation = XCTestExpectation(description: "expect changes for publisher")
+        expectation.isInverted = true
+        var bag = Set<AnyCancellable>()
+        publisher.sink { _ in
+            callCount += 1
+            expectation.fulfill()
+        }
+        .store(in: &bag)
+        
+        try callback()
+        
+        wait(for: [expectation], timeout: 1)
+
+        XCTAssertEqual(callCount, 0, file: file, line: line)
     }
 
 }
