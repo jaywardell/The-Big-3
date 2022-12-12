5aee3197d8ea155376c67b30b7a4e6c222a12588
Joseph Wardell <jaywardell@mac.com>
2022-12-12 11:16:05 -0500

    more cleanup

--- The_Big_3_Widgets/The_Big_3_Widgets.swift
+++ The_Big_3_Widgets/The_Big_3_Widgets.swift
@@ -10,13 +10,13 @@ import SwiftUI
 import Intents
 
 
-struct The_Big_3_WidgetsEntryView : View {
-    var entry: TheBig3TimelineProvider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
-}
+//struct The_Big_3_WidgetsEntryView : View {
+//    var entry: TheBig3TimelineProvider.Entry
+//
+//    var body: some View {
+//        Text(entry.date, style: .time)
+//    }
+//}
 
 @main
 struct The_Big_3_Widgets: Widget {
@@ -26,19 +26,11 @@ struct The_Big_3_Widgets: Widget {
     
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: TheBig3TimelineProvider(model: model)) { entry in
-//            The_Big_3_WidgetsEntryView(entry: entry)
             TheBig3WidgetView(entry: entry)
                 .accentColor(ViewConstants.tint)
         }
         .supportedFamilies([.systemSmall, .systemMedium, .systemLarge, .systemExtraLarge])
         .configurationDisplayName("The Big 3")
         .description("Track your progress on your Big 3")
-    }
-}
-
-struct The_Big_3_Widgets_Previews: PreviewProvider {
-    static var previews: some View {
-        The_Big_3_WidgetsEntryView(entry: Entry(planner: Planner(plan: .example), date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
