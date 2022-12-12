8831368e3656497cc4207d901814c50e0361e1dd
Joseph Wardell <jaywardell@mac.com>
2022-12-12 11:44:50 -0500

    added a watch app target to the project and added an app group capability

---
+++ The_Big_3_watch Watch App/The_Big_3_watchApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Big_3_watchApp.swift
+//  The_Big_3_watch Watch App
+//
+//  Created by Joseph Wardell on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Big_3_watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
