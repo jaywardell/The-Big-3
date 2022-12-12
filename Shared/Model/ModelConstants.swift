7e5da9e875a903d299881d955bb439c2f82d2b0e
Joseph Wardell <jaywardell@mac.com>
2022-12-12 09:36:48 -0500

    added a note about app groups for anyone trying to use the code

--- Shared/Model/ModelConstants.swift
+++ Shared/Model/ModelConstants.swift
@@ -10,5 +10,10 @@ import Foundation
 enum ModelConstants {
     static var allowedGoalsPerPlan: Int { 3 }
     
+    // NOTE: if you try to use this module with any provisioning profile other than the author's
+    // it will not work.
+    // You will need to set up a new app group associated with your provisioning profile
+    // in App Store Connect.
+    // see https://developer.apple.com/documentation/xcode/configuring-app-groups
     static var appGroup: String { "group.com.oldjewel.TheBig3" }
 }
