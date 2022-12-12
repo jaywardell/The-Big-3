c97e95fe3baa24dbfc14e37cd0e1c179de481e25
Joseph Wardell <jaywardell@mac.com>
2022-12-12 11:06:52 -0500

    added a watch target. included the model objects I should need, and made it show a view that just prompts the user to plan their big 3 on the iPhone

---
+++ The_Big_3_Watch Watch App/The_Big_3_WatchApp.swift
@@ -0,0 +1,18 @@
+//
+//  The_Big_3_WatchApp.swift
+//  The_Big_3_Watch Watch App
+//
+//  Created by Joseph Wardell on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct The_Big_3_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NoPlanView()
+                .accentColor(ViewConstants.tint)
+        }
+    }
+}
