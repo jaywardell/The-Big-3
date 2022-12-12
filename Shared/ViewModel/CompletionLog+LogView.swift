b87a8dfb3ceaf7b54dd1b017049908cdf936275a
Joseph Wardell <jaywardell@mac.com>
2022-12-12 09:25:27 -0500

    removed extraneous self references and cleaned up code for reusability

--- Shared/ViewModel/CompletionLog+LogView.swift
+++ Shared/ViewModel/CompletionLog+LogView.swift
@@ -11,21 +11,24 @@ extension CompletionLog {
     
     func historyViewModel() -> LogView.ViewModel {
         
-        return .init(publisher: logChanged.receive(on: RunLoop.main).eraseToAnyPublisher(),
-              days: {
-            Task {
-                await self.loadArchive()
-            }
-
-            return self.days } )
-        { day in
-            
-            
-            return self.timesForGoals(completedOn: day).compactMap {
-                guard let goal = self.titleForGoal(completedAt: $0) else { return nil }
-                return LogEntryRow.ViewModel(time: $0, goal: goal)
-            }
-        }
+        return LogView.ViewModel.init(publisher: logChanged.receive(on: RunLoop.main).eraseToAnyPublisher(),
+                                      days: retrieveDays,
+                                      goalsForDay: retrieveGoals(for:))
     }
     
+    fileprivate func retrieveDays() -> [Date] {
+        
+        Task { [weak self] in
+            await self?.loadArchive()
+        }
+        
+        return days
+    }
+    
+    fileprivate func retrieveGoals(for day: Date) -> [LogEntryRow.ViewModel] {
+        timesForGoals(completedOn: day).compactMap {
+            guard let goal = titleForGoal(completedAt: $0) else { return nil }
+            return LogEntryRow.ViewModel(time: $0, goal: goal)
+        }
+    }
 }
