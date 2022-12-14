f8a8b07e1dade0bbd371468709e69416e3ee6f4b
Joseph Wardell <jaywardell@mac.com>
2022-12-14 13:36:50 -0500

    added a .gitignore file. added a (git-ignored) text file that stores the app group id. ModelConstants.appGroup loads that file and returns the string in it. added documentation to explain to other developers the steps necessary to keep ModelConstants.appGroup from crashing

--- App/Model/ModelConstants.swift
+++ App/Model/ModelConstants.swift
@@ -10,12 +10,22 @@ import Foundation
 enum ModelConstants {
     static var allowedGoalsPerPlan: Int { 3 }
     
-    // NOTE: if you try to use this module with any provisioning profile other than the author's
-    // it will not work.
+    // NOTE: the file referenced here isn't included in this repo.
     // You will need to set up a new app group associated with your provisioning profile
     // in App Store Connect.
-    // see https://developer.apple.com/documentation/xcode/configuring-app-groups
-    static var appGroup: String { "group.com.oldjewel.TheBig3" }
+    // (see https://developer.apple.com/documentation/xcode/configuring-app-groups)
+    //
+    // and then paste the group id into a file called "group_id.txt"
+    // which you then set to be copied into your bundle's resources.
+    // (the App/Model/Resources directory is probably the best place)
+    // you will also have to change the group id
+    // in the app target's "Signing & Capabilities" tab
+    // and also in the widget target's "Signing & Capabilities"
+    // to the group id you created.
+    static var appGroup: String {
+        let fileURL = Bundle.main.url(forResource: "group_id", withExtension: "txt")!
+        return try! String(contentsOf: fileURL).trimmingCharacters(in: .whitespacesAndNewlines)
+    }
     
     static var WatchConnectivityPlanKey: String { #function }
     static var WatchConnectivityCompletedGoalKey: String { #function }
