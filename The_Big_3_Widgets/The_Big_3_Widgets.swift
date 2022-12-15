3c1fbd7d221aea7ef15785aa308db7316bbda7e8
Joseph Wardell <jaywardell@mac.com>
2022-12-15 10:33:48 -0500

    refactoring to get rid of unnecessary context on init

--- The_Big_3_Widgets/The_Big_3_Widgets.swift
+++ The_Big_3_Widgets/The_Big_3_Widgets.swift
@@ -20,7 +20,7 @@ struct The_Big_3_Widgets: Widget {
         IntentConfiguration(kind: kind,
                             intent: ConfigurationIntent.self,
                             provider: WidgetTimelineProvider(model: model)) { entry in
-            WidgetView(entry: entry)
+            WidgetView(planner: entry.planner)
                 .accentColor(ViewConstants.tint)
         }
                             .supportedFamilies([.systemSmall, .systemMedium, .systemLarge, .systemExtraLarge, .accessoryRectangular, .accessoryInline, .accessoryCircular])
--- The_Big_3_Widgets/View/WidgetView.swift
+++ The_Big_3_Widgets/View/WidgetView.swift
@@ -9,27 +9,23 @@ import SwiftUI
 import WidgetKit
 
 struct WidgetView: View {
-    let entry: Entry
+    let planner: Planner
 
     var body: some View {
-        switch entry.planner.state {
+        switch planner.state {
         case .planning: PlanPromptWidgetView()
-        case .doing: WidgetPlanView(planner: entry.planner)
-        case .finished: WidgetSummationView(viewModel: entry.planner.summationViewModel())
+        case .doing: WidgetPlanView(planner: planner)
+        case .finished: WidgetSummationView(viewModel: planner.summationViewModel())
         }
     }
 }
 
 struct TheBig3WidgetView_Previews: PreviewProvider {
     static var previews: some View {
-        WidgetView(entry: Entry(planner: Planner(plan: .example2),
-                          date: Date(),
-                          configuration: ConfigurationIntent()))
+        WidgetView(planner: Planner(plan: .example2))
         .previewContext(WidgetPreviewContext(family: .systemSmall))
 
-        WidgetView(entry: Entry(planner: Planner(plan: Plan(allowed: 3)),
-                          date: Date(),
-                          configuration: ConfigurationIntent()))
+        WidgetView(planner: Planner(plan: Plan(allowed: 3)))
         .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
